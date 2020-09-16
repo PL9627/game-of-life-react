b52968633b46284f2c20ddbf7280bdd06ac4e0f4
Paul Lee <paul.lee0627@gmail.com>
2020-09-15 18:27:09 -0700

    did some work to BoardGrid

--- game-of-life-react/src/game.js
+++ game-of-life-react/src/game.js
@@ -18,7 +18,31 @@ const newBoardStatus = (cellStatus = () => Math.random() < 0.3) => {
   return grid;
 };
 
-const BoardGrid = () => {};
+const BoardGrid = ({ boardStatus, onToggleCellStatus }) => {
+  const handleClick = (r, c) => onToggleCellStatus(r, c);
+
+  const tr = [];
+
+  for (let r = 0; r < gridRows; r++) {
+    const td = [];
+
+    for (let c = 0; c < gridCols; c++) {
+      td.push(
+        <td
+          key={`${r}, ${c}`}
+          className={boardStatus[r][c] ? "alive" : "dead"}
+          onClick={() => handleClick(r, c)}
+        />
+      );
+    }
+    tr.push(<tr key={r}>{td}</tr>);
+  }
+  return (
+    <table>
+      <tbody>{tr}</tbody>
+    </table>
+  );
+};
 
 const Slider = () => {};
 
