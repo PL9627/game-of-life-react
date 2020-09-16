df20672d7519eafce9c52132180a29831efcf2d5
Paul Lee <paul.lee0627@gmail.com>
2020-09-15 18:09:22 -0700

    imported rules into App.js

--- game-of-life-react/src/App.js
+++ game-of-life-react/src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Rules from "./rules";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
