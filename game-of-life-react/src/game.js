72cdced2e22b514899e40ecd3bcbb3b775661880
Paul Lee <paul.lee0627@gmail.com>
2020-09-15 18:19:00 -0700

    added code to newBoaedStatus to create a new grid

--- game-of-life-react/src/game.js
+++ game-of-life-react/src/game.js
@@ -5,20 +5,29 @@ import React, { Component } from "react";
 const gridRows = 300;
 const gridCols = 400;
 
-const newBoardStatus = () => {}
+const newBoardStatus = (cellStatus = () => Math.random() < 0.3) => {
+  const grid = [];
 
-const BoardGrid = () => {}
+  for (let r = 0; r < gridRows; r++) {
+    grid[r] = [];
 
-const Slider = () => {}
+    for (let c = 0; c < gridCols; c++) {
+      grid[r][c] = cellStatus();
+    }
+  }
+  return grid;
+};
+
+const BoardGrid = () => {};
+
+const Slider = () => {};
 
 class Game extends Component {
-    state = {};
+  state = {};
 
-    render() {
-        return(
-            <div></div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default Game;
