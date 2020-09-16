c9567d84fa5e112741b54d153b1e55b40c7ee613
Paul Lee <paul.lee0627@gmail.com>
2020-09-15 18:14:58 -0700

    started on game.js

---
+++ game-of-life-react/src/game.js
@@ -0,0 +1,24 @@
+const { Component } = require("react");
+
+import React, { Component } from "react";
+
+const gridRows = 300;
+const gridCols = 400;
+
+const newBoardStatus = () => {}
+
+const BoardGrid = () => {}
+
+const Slider = () => {}
+
+class Game extends Component {
+    state = {};
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Game;
