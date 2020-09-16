78017d98c9d6ade20896555fbfada41423a385f4
Paul Lee <paul.lee0627@gmail.com>
2020-09-15 18:02:55 -0700

    started on the rules

---
+++ game-of-life-react/src/rules.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Rules() {
+    return (
+        <div className="rules">
+            <h1>Rules to Conway's Game of Life</h1>
+            <p>The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, 
+            each of which is in one of two possible states, live or dead, (or populated and unpopulated, respectively). 
+            Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.</p>
+        </div>
+    )
+}
