7ca31055b2d05abcc5d8d319abda4b2f0b64d82e
Paul Lee <paul.lee0627@gmail.com>
2020-09-17 16:23:31 -0700

    added Game and Rules to App.js

--- game-of-life-react/src/App.js
+++ game-of-life-react/src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 
 import Rules from "./rules";
+import Game from "./game";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Game />
+        <Rules />
+      </header>
     </div>
   );
 }
