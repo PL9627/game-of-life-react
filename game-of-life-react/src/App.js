d3677df9a418992f6f47961163b098faf1ee2304
Paul Lee <paul.lee0627@gmail.com>
2020-09-17 17:13:33 -0700

    everything but grid appears

--- game-of-life-react/src/App.js
+++ game-of-life-react/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./App.css";
 
-import Rules from "./rules";
-import Game from "./game";
+import Rules from "./rules/rules";
+import Game from "./game/game";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Game />
-        <Rules />
-      </header>
+      <Game />
+      <Rules />
     </div>
   );
 }
